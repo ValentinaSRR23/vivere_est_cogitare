cb2ba32003afe4c4287246cc83fab50623e7e3f5
Valentina <valentinaserrao23@gmail.com>
2021-01-10 20:28:15 +0100

    publish

--- Logics/autogenWorks.js
+++ Logics/autogenWorks.js
@@ -205,22 +205,30 @@ var DocuVale = [
     "content": [
         {
             "type": "text",
-            "value": "Riga di testo ma che bello che bello che sono felice da morire quanto sono contento"
+            "value": "Insieme alle strade, gli acquedotti sono state le grandi opere d'ingegneria  con cui i Romani hanno stupito il mondo.<br>"
         },
         {
             "type": "text",
-            "value": "Altro testo molto carino"
+            "value": "La costruzione degli acquedotti fu una delle imprese più grandi e più impegnative  della civiltà romana, \"la più alta manifestazione della grandezza di Roma\",  come scrisse nel 97 d.C. Frontino in qualità di \"curatore degli acquedotti\"  (<em>\"curator aquarum\"</em>), nel suo trattato <em>\"De aquae ductu urbis Romae\"</em>,  ovvero \"Gli acquedotti della città di Roma\"."
+        },
+        {
+            "type": "text",
+            "value": "Per secoli il Tevere, le sorgenti e i pozzi furono in grado di soddisfare il fabbisogno  della città finché lo sviluppo urbanistico e la crescita demografica resero necessario  ricorrere ad altre fonti: fu allora che, grazie all'abilità dei suoi costruttori,  si realizzarono gli acquedotti. Da quel momento in poi, ovvero dal 312 a.C.,  affluì a Roma una quantità enorme di acqua potabile, come nessun'altra città del mondo  antico, ma forse di ogni epoca, ebbe mai e che valse alla città il titolo  di <em>\"regina aquarum\"</em>, ossia <strong>\"regina delle acque\"</strong>. <br> <br>"
+        },
+        {
+            "type": "text",
+            "value": "Per maggiori informazioni, ho realizzato insieme ad una mia collega un video illustrativo,  ricco di dettagli e spiegazioni, che sarà in grado di trasportarvi direttamente  nell'Antica Roma. <br> Buona visione. <br> <a src=\"https://www.youtube.com/watch?v=KlDXETuqZhc&t=94s\">  Link: Gli acquedotti, arterie del cuore pulsante dell'antica Roma  </a>"
         },
         {
             "type": "video",
-            "value": "http://youtube.com/video_super_carino"
+            "value": "https://www.youtube.com/embed/KlDXETuqZhc&t=94s"
         },
         {
             "type": "text",
-            "value": "Testo conclusivo"
+            "value": "“È questa la terza opera degli antichi che ho innanzi a me e di cui osservo  la stessa impronta, sempre grandiosa: l'arte architettonica degli antichi è veramente  una seconda natura, che opera conforme agli usi e agli scopi civili.  È così che sorge l'anfiteatro, il tempio, <strong>l'acquedotto</strong>.  E soltanto adesso sento con quanta ragione ho sempre trovato detestabili le costruzioni  fatte a capriccio […]. Cose tutte nate morte, poiché ciò che veramente non ha in sé  una ragione di esistere non ha vita e non può essere grande, né diventare grande.” <br> —  Johann Wolfgang von Goethe drammaturgo, poeta, saggista, scrittore, pittore,  teologo, filosofo, umanista, scienziato, critico d'arte. (1749 - 1832)"
         }
     ],
     "type": "Gregorio",
-    "title": "Un lavoro con gregorio",
+    "title": "Gli acquedotti, arterie del cuore pulsante dell'antica Roma.",
     "preview_image": "./Acquedotti/Storytelling/A1.png"
 }];
